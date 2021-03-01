c1a0b431c909c6bb8192c0250277cbf1fb9c704a
etarter <etarter@student.ethz.ch>
2021-03-01 19:04:12 +0100

    cleaned up workflow

--- workflow/3DDLC/3ddlc-WORKFLOW.py
+++ workflow/3DDLC/3ddlc-WORKFLOW.py
@@ -2,37 +2,32 @@ import deeplabcut
 import tkinter
 from tkinter import filedialog
 import os
-import glob
-from mpl_toolkits.mplot3d import Axes3D
+from glob import glob
 import subprocess
 import pandas as pd
 from pathlib import Path
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-#change everything to a class
+def file_dialog(type, text):
 
-
-def file_dialog(type):
     root = tkinter.Tk()
     root.geometry('0x0+0+0')
     root.lift()
     root.focus_force()
     root.deiconify()
     root.update_idletasks()
-    if type == 'file':
-        path = filedialog.askopenfilename(parent=root, initialdir= os.getcwd(), title= "config_path")
-    elif type == 'file3d':
-        path = filedialog.askopenfilename(parent=root, initialdir= os.getcwd(), title= "config_path3d")
-    elif type == 'filema':
-        path = filedialog.askopenfilename(parent=root, initialdir= os.getcwd(), title= "config_pathma")
-    elif type == 'directory':
-        path = filedialog.askdirectory(parent=root, initialdir=os.getcwd(), title='videos_dir')
-    elif type == 'vfile':
-        path = filedialog.askopenfilename(parent=root, initialdir=os.getcwd(), title='video_path')
+    if type == 'f':
+        path = filedialog.askopenfilename(parent=root, initialdir= os.getcwd(), title=text)
+    elif type == 'dir':
+        path = filedialog.askdirectory(parent=root, initialdir=os.getcwd(), title=text)
     root.withdraw()
+
     return path
 
 
 def open_project():
+
     print('(n) new project\n(l) load existing')
     console = input()
     loop = True
@@ -44,72 +39,50 @@ def open_project():
             print('your_name')
             your_name = input()
             print('videos_dir')
-            videos_dir = os.path.join(file_dialog('directory'))
-            config_path = deeplabcut.create_new_project('sa-'+project_name, your_name, [videos_dir], videotype='.mp4', copy_videos=False, multianimal=False)
+            videos_dir = os.path.join(file_dialog('dir', 'select video directory'))
             config_pathma = deeplabcut.create_new_project(project_name, your_name, [videos_dir], videotype='.mp4', copy_videos=False, multianimal=True)
             config_path3d = deeplabcut.create_new_project_3d(project_name, your_name, num_cameras=2)
             print('edit config files')
-            subprocess.call([r'C:\Users\etarter\AppData\Local\atom\atom.exe', config_path])
             subprocess.call([r'C:\Users\etarter\AppData\Local\atom\atom.exe', config_pathma])
             subprocess.call([r'C:\Users\etarter\AppData\Local\atom\atom.exe', config_path3d])
             loop = False
-        elif console == 'l': #change that user is asked which project he wants to load
+
+        elif console == 'l':
             model = 'test'
-            if model == 'hybrid':
-                config_path = r'C:\Users\etarter\Downloads\dlc\hybrid-dlc-2020-11-23\config.yaml'#os.path.join(file_dialog('file'))
-                config_path3d = r'C:\Users\etarter\Downloads\dlc\hybrid-dlc-2020-11-23-3d\config.yaml'#os.path.join(file_dialog('file3d'))
-                videos_dir = r'C:\Users\etarter\Downloads\videos'
-            elif model == 'full3dma':
-                # full3dma 2d config file must point to hybrid model config file
-                # full3dma 3d config file must have link to hybrid model config file
-                config_path = r'C:\Users\etarter\Downloads\dlc\hybrid-dlc-2020-11-23\config.yaml'
-                config_pathma = r'C:\Users\etarter\Downloads\dlc\full3dma-dlc-2020-11-23\config.yaml'
-                config_path3d = r'C:\Users\etarter\Downloads\dlc\full3dma-dlc-2020-11-23-3d\config.yaml'
-                videos_dir = r'C:\Users\etarter\Downloads\videos'
-            elif model == 'test':
-                # test 2d config file must point to test1 model config file
-                # test1 3d config file must have link to test1 model config file
-                config_path = r'C:\Users\etarter\Downloads\dlc\test1-dlc-2021-01-04\config.yaml'
+
+            if model == 'test':
                 config_pathma = r'C:\Users\etarter\Downloads\dlc\test-dlc-2021-01-04\config.yaml'
                 config_path3d = r'C:\Users\etarter\Downloads\dlc\test1-dlc-2021-01-05-3d\config.yaml'
                 videos_dir = r'C:\Users\etarter\Downloads\videos'
-            elif model == 'obs':
-                # test 2d config file must point to test1 model config file
-                # test1 3d config file must have link to test1 model config file
-                config_path = r'C:\deeplabcut\dlc\sa-observation-pdz-2021-01-13\config.yaml'
-                config_pathma = r'C:\deeplabcut\dlc\observation-pdz-2021-01-13\config.yaml'
-                config_path3d = r'C:\deeplabcut\dlc\observation-pdz-2021-01-13-3d\config.yaml'
-                videos_dir = r'C:\deeplabcut\videos\12-01-21-model-creation'
-            elif model == 'calib':
-                # test 2d config file must point to test1 model config file
-                # test1 3d config file must have link to test1 model config file
-                config_path = r'C:\Users\etarter\Downloads\dlc\sa-calib-dlc-2021-02-10\config.yaml'
-                config_pathma = r'C:\Users\etarter\Downloads\dlc\calib-dlc-2021-02-10\config.yaml'
-                config_path3d = r'C:\Users\etarter\Downloads\dlc\calib-dlc-2021-02-10-3d\config.yaml'
-                videos_dir = r'C:\Users\etarter\Downloads\videos-new'
+
             elif model == 'ask':
-                config_path = os.path.join(file_dialog('file'))
-                config_pathma = os.path.join(file_dialog('filema'))
-                config_path3d = os.path.join(file_dialog('file3d'))
-                videos_dir = os.path.join(file_dialog('directory'))
+                config_pathma = os.path.join(file_dialog('f', 'select multianimal config file'))
+                config_path3d = os.path.join(file_dialog('f', 'select 3d config file'))
+                videos_dir = os.path.join(file_dialog('dir', 'select videos dir'))
             loop = False
+
         else:
             print('try again')
             console = input()
-    return config_path, config_pathma, config_path3d, videos_dir
+
+    return config_pathma, config_path3d, videos_dir
 
 
-def process(config_path, config_pathma, config_path3d, videos_dir):
+def process(config_pathma, config_path3d, videos_dir):
+
     loop = True
-    what_to_do = '(e) extract frames\n(l) label frames\n(t) create, train and evaluate network \n(a) analyze videos\n(c) triangulate\n(p) create labeled video\n(r) extract outliers and refine labels\n(m) merge and retrain network\n(v) add new videos\n(x) exit\n'
+    what_to_do = '(e) extract frames\n(l) label frames\n(t) create, train and evaluate network \n(a) analyze videos\n(rt) refine tracklets or (dt) convert detections to tracklets\n(f) filter predictions\n(c) triangulate\n(p) create labeled video\n(r) extract outliers and refine labels\n(m) merge and retrain network\n(v) add new videos\n(x) exit\n'
     answer = input(what_to_do)
+
     while loop:
         if answer == 'e':
             deeplabcut.extract_frames(config_pathma, mode='automatic', algo='kmeans', userfeedback=False, cluster_resizewidth=10, cluster_step=1)
             answer = input(what_to_do)
+
         elif answer == 'l':
             deeplabcut.label_frames(config_pathma)
             answer = input(what_to_do)
+
         elif answer == 't':
             deeplabcut.create_multianimaltraining_dataset(config_pathma, net_type='resnet_50')
             iterations = input('max iterations: ')
@@ -123,40 +96,93 @@ def process(config_path, config_pathma, config_path3d, videos_dir):
                     }
             deeplabcut.evaluate_multianimal_crossvalidate(config_pathma, pbounds=pbounds, target='rpck_test')
             answer = input(what_to_do)
+
         elif answer == 'a':
             deeplabcut.analyze_videos(config_pathma, [videos_dir], videotype='.mp4', gputouse=0, save_as_csv=False)
+            answer = input(what_to_do)
+
+        elif answer == 'rt':
             deeplabcut.convert_detections2tracklets(config_pathma, [videos_dir], videotype='.mp4', track_method='skeleton')
-            videos = range(len(glob.glob(videos_dir+'\*_sk.pickle')))
-            for video in videos:
-                man, viz = deeplabcut.refine_tracklets(config_pathma, os.path.join(videos_dir, glob.glob(videos_dir+'\*_sk.pickle')[video]), os.path.join(file_dialog('vfile')))
+            pickles = glob(videos_dir+'/*_sk.pickle')
+            pickles.sort()
+            videos = glob(videos_dir+'/*.mp4')
+            videos.sort()
+            shape = range(len(pickles))
+
+            for i in shape:
+                man, viz = deeplabcut.refine_tracklets(config_pathma, pickles[i], videos[i])
+
             answer = input(what_to_do)
+
+        elif answer == 'dt':
+            deeplabcut.convert_detections2tracklets(config_pathma, [videos_dir], videotype='.mp4', track_method='skeleton')
+            pickles = glob(videos_dir+'/*_sk.pickle')
+            shape = range(len(pickles))
+
+            for i in shape:
+                deeplabcut.convert_raw_tracks_to_h5(config_pathma, pickles[i])
+
+            answer = input(what_to_do)
+
+        elif answer == 'f':
+            deeplabcut.filterpredictions(config_pathma, [videos_dir], videotype='.mp4', filtertype='arima', track_method='skeleton', save_as_csv=False)
+            answer = input(what_to_do)
+
         elif answer == 'c':
-            vids = range(len(glob.glob(videos_dir+'\*_sk.h5')))
-            
-            for vid in vids:
-                ma_h5 = pd.read_hdf(glob.glob(videos_dir+'\*_sk.h5')[vid])
-                sa_h5 = pd.read_hdf(glob.glob(str(Path(videos_dir).parent.parent)+'\other\*_'+str(iterations)+'.h5')[vid])
-                outfile = os.path.join(videos_dir, os.path.basename(glob.glob(str(Path(videos_dir).parent.parent)+'\other\*'+str(iterations)+'.h5')[vid]))
+            try:
+                vids = glob(videos_dir+'/*_filtered.h5')
 
+                for vid in vids:
+                    tracking_file = pd.read_hdf(vid)
+                    outfile = '_'.join(vid.split('_')[:-2])
+                    columns = tracking_file.columns.to_frame(index=False)
+                    shape = range(columns.shape[0])
 
+                    for row in shape:
+                        columns.bodyparts.iloc[row] = columns.individuals.iloc[row]+'_'+columns.bodyparts.iloc[row]
 
+                    columns_syn = pd.MultiIndex.from_frame(columns.drop(columns=['individuals']))
+                    tracking_file.to_hdf(outfile+'_original.h5', key="df_with_missing", mode="w")
+                    tracking_file.columns = columns_syn
+                    tracking_file.to_hdf(outfile+'_synthetic.h5', key="df_with_missing", mode="w")
+                    tracking_file.to_hdf(outfile+'.h5', key="df_with_missing", mode="w")
 
+            except AttributeError:
+                print('files already modified!')
 
-
-
-
-
-                ma_h5.columns = sa_h5.columns
-                ma_h5.to_hdf(outfile, key="df_with_missing", mode="w")
             deeplabcut.triangulate(config_path3d, videos_dir, videotype='.mp4', gputouse=0, filterpredictions=True)
+            vids_3d = glob(videos_dir+'/*3D.h5')
+
+            for vid_3d in vids_3d:
+                tracking_file_3d = pd.read_hdf(vid_3d)
+                outfile_3d = vid_3d.split('.')[:-1][0]
+                columns_3d = tracking_file_3d.columns.to_frame(index=False)
+                individuals = []
+                shape_3d = range(columns_3d.shape[0])
+
+                for row in shape_3d:
+                    id = columns_3d.bodyparts.iloc[row].split('_')[:1][0]
+                    bp = '_'.join(columns_3d.bodyparts.iloc[row].split('_')[1:])
+                    columns_3d.bodyparts.iloc[row] = bp
+                    individuals.append(id)
+
+                columns_3d.insert(1, 'individuals', individuals)
+                columns_3d_syn = pd.MultiIndex.from_frame(columns_3d)
+                tracking_file_3d.to_hdf(outfile_3d+'_original.h5', key="df_with_missing", mode="w")
+                tracking_file_3d.columns = columns_3d_syn
+                tracking_file_3d.to_hdf(outfile_3d+'_synthetic.h5', key="df_with_missing", mode="w")
+
             answer = input(what_to_do)
+
         elif answer == 'p':
             deeplabcut.create_labeled_video_3d(config_path3d, [videos_dir], videotype='.mp4', trailpoints=10, view=[0,270])
             answer = input(what_to_do)
+
         elif answer == 'r':
             deeplabcut.extract_outlier_frames(config_pathma, [videos_dir], videotype='.mp4', extractionalgorithm='kmeans', cluster_resizewidth=10, automatic=True, cluster_color=True, track_method='box')
             deeplabcut.refine_labels(config_pathma)
             answer = input(what_to_do)
+
         elif answer == 'm':
             print('max iterations')
             iterations = input()
@@ -166,42 +192,50 @@ def process(config_path, config_pathma, config_path3d, videos_dir):
             deeplabcut.evaluate_network(config_pathma, gputouse=0, plotting=True)
             deeplabcut.evaluate_multianimal_crossvalidate(config_pathma, plotting=True)
             answer = input(what_to_do)
+
         elif answer == 'v':
-            new_videos_dir = os.path.join(file_dialog('vfile'))
+            new_videos_dir = os.path.join(file_dialog('f', 'select video file'))
             deeplabcut.add_new_videos(config_pathma, [new_videos_dir], copy_videos=False)
             answer = input(what_to_do)
+
         elif answer == 'x':
             loop = False
+
         else:
             answer = input('error: ')
 
 
-def print_config(config_path, config_pathma, config_path3d, videos_dir):
-    print('config_path\t', config_path)
+def print_config(config_pathma, config_path3d, videos_dir):
+
     print('config_pathma\t', config_pathma)
     print('config_path3d\t', config_path3d)
     print('videos_dir\t', videos_dir)
 
-config_path, config_pathma, config_path3d, videos_dir = open_project()
+config_pathma, config_path3d, videos_dir = open_project()
 
-def run_process(config_path, config_pathma, config_path3d, videos_dir):
+def run_process(config_pathma, config_path3d, videos_dir):
+
     loop = True
     what_to_do = '(p) config paths\n(w) workflow\n(x) exit'
     print(what_to_do)
     console = input()
+
     while loop:
         if console == 'p':
-            print_config(config_path, config_pathma, config_path3d, videos_dir)
+            print_config(config_pathma, config_path3d, videos_dir)
             print(what_to_do)
             console = input()
+
         elif console == 'w':
-            process(config_path, config_pathma, config_path3d, videos_dir)
+            process(config_pathma, config_path3d, videos_dir)
             print(what_to_do)
             console = input()
+
         elif console == 'x':
             loop = False
+
         else:
             print('error')
             console = input()
 
-run_process(config_path, config_pathma, config_path3d, videos_dir)
+run_process(config_pathma, config_path3d, videos_dir)
